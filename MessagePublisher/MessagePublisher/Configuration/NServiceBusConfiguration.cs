a00bf52a31a93c479548b6fe5e4290528e459b3a
0pens0 <penso.oren@gmail.com>
2020-12-01 11:33:21 +0000

    commit

--- MessagePublisher/MessagePublisher/Configuration/NServiceBusConfiguration.cs
+++ MessagePublisher/MessagePublisher/Configuration/NServiceBusConfiguration.cs
@@ -47,7 +47,7 @@ namespace MessagePublisher.Configuration
             subscriptions.SubscriptionTableName(
                 tableName: "SubscriptionRouting",
                 schemaName: "dbo",
-                catalogName: "Message.NServiceBus");
+                catalogName: "Message_NServiceBus");
 
             var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
             persistence.ConnectionBuilder(() => new SqlConnection(messagePublisherConnection));
